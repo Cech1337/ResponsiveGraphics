3c6c57ef2602cf3bb89b83eb96024df0841e8506
maxluster <maxluster@gmail.com>
2013-01-16 14:33:02 -0800

    Init commit

---
+++ javascripts/main.js
@@ -0,0 +1,22 @@
+//Inits
+$('p, table').moltenLeading({
+	minline: 1.2,
+	maxline: 1.5,
+	minwidth: 320,
+	maxwidth: 460
+});
+
+$('.caption').moltenLeading({
+	minline: 1.1,
+	maxline: 1.3,
+	minwidth: 320,
+	maxwidth: 600
+});
+
+$(document).ready(function() {
+
+});
+
+$(window).load(function() {
+
+});
