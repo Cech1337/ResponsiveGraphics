2fdc4b1bcbc04a3818bdf34524ef5ae95c7afdf0
maxluster <maxluster@gmail.com>
2013-02-06 16:06:18 -0800

    Added sparklines, style refinements

--- javascripts/main.js
+++ javascripts/main.js
@@ -15,6 +15,9 @@ $('.caption').moltenLeading({
 
 $(document).ready(function() {
 
+	var myvalues = [10,8,5,7,4,4,1,10,8,5,7,4,4,1,10,8,5,7,4,4,1];
+    $('.dynamicsparkline').sparkline(myvalues);
+
 });
 
 $(window).load(function() {
