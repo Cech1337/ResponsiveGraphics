3fe99af6d0e545dfe89ae0bcfbf1ef1708a8a3f1
maxluster <maxluster@gmail.com>
2013-02-12 17:10:30 -0800

    Latest

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,12 +14,34 @@ $('.caption').moltenLeading({
 });
 
 $(document).ready(function() {
-
 	var myvalues = [10,8,5,7,4,4,1,10,8,5,7,4,4,1,10,8,5,7,4,4,1];
     $('.dynamicsparkline').sparkline(myvalues);
+
+    //Enclose all table cells in spans
+    $('td, th').wrapInner('<span />');
 
 });
 
 $(window).load(function() {
+
+});
+
+
+$(window).on("throttledresize", function( event ) {
+
+
+	// $("#variable-size-table span").each(function(){
+
+
+	// 	var contentWidth = $(this).width(); 
+	// 	var columnWidth = $(this).parent().width();
+	// 	if(columnWidth < contentWidth - 12){
+	// 		$("#variable-size-table").addClass("reduce-1");
+	// 	}
+
+	// });
+
+	//Transverse 
+
 
 });
