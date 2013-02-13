5fe9f7fef22e7b2a09b46643806380e255a077da
maxluster <maxluster@gmail.com>
2013-02-13 14:37:13 -0800

    Bidirectional resizing works

--- javascripts/main.js
+++ javascripts/main.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
 
     //Enclose all table cells in spans so we can calculate colision distance
     $('td, th').wrapInner('<span />');
+    $(".scalable").attr("reduceFactor", 0);
 
 });
 
@@ -27,41 +28,56 @@ $(window).load(function() {
 });
 
 
-
-var reduceBy = 0;
-
 $(window).on("throttledresize", function(event){
 
-	var resized = false;
-	var minDistance;
+	//Loop through each scalable table
+	$(".scalable").each(function(){
 
-	$(".scalable table span").each(function(){
-		var contentWidth = $(this).width(); 
-		var columnWidth = $(this).parent().width();
-		var distance = columnWidth - contentWidth;
+		var scalableContainer = $(this);
+		var reduceFactor = $(this).attr("reduceFactor");
+		var resized = false;
+		var minDistance;
 
-		if(minDistance == null || distance < minDistance){
-			minDistance = distance;
+		// Loop through this table's content to detect potential collisions
+		$(this).find("table span").each(function(){
+
+			var contentWidth = $(this).width(); 
+			var columnWidth = $(this).parent().width();
+			var distance = columnWidth - contentWidth;
+
+			if(minDistance == null || distance < minDistance){
+				minDistance = distance;
+			}
+
+			if(distance < 10){
+				//Remove existing reduce class
+				$(scalableContainer).removeClass("reduce" + reduceFactor);
+				//Set reduce factor one less than current
+				reduceFactor--;
+				//Set attribute to reflect scale change
+				$(scalableContainer).attr("reduceFactor", reduceFactor);
+				//Add a class to render scale change
+				$(scalableContainer).addClass("reduce" + reduceFactor);
+				//Don't attempt to increase size on this loop
+				resized = true;
+				//break
+				return false;
+			}
+		});
+
+		//If all cells have plenty of breathing room, expand font sizes until back to normal
+		if(!resized && minDistance > 30 && reduceFactor < 0){
+			//Remove existing reduce class
+			$(scalableContainer).removeClass("reduce" + reduceFactor);
+			//Set reduce factor one more than current
+			reduceFactor++;
+			//Set attribute to reflect scale change
+			$(scalableContainer).attr("reduceFactor", reduceFactor);
+			//Add a class to render scale change
+			$(scalableContainer).addClass("reduce" + reduceFactor);
 		}
 
-		console.log(
-			"Content: " + contentWidth +"\n"+
-			"Column: " 	+ columnWidth +"\n"+
-			"Distance: "+ distance
-		);
-
-		if(distance < 12){
-			$(".scalable").removeClass("reduce" + reduceBy);
-			reduceBy--;
-			$(".scalable").addClass("reduce" + reduceBy);
-			resized = true;
-			return false;
-		}
 	});
 
-	//If cells have plenty of breathing room, expand font sizes until back to normal
-	if(!resized && minDistance > 40 && reduceBy < 0){
-
-	}
 
 });
